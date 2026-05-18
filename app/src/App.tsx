3c45cd0f4792cf43a364163678168ee6fa91ce8c
BoeunHan <h.boeunn@gmail.com>
2026-05-18 23:43:45 +0900

    feat: react 프로젝트 초기 생성

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App(): React.ReactElement {
+  return (
+    <div className="App">
+      <h1>Welcome to React</h1>
+      <p>This is your React application</p>
+    </div>
+  );
+}
+
+export default App;
